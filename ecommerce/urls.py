78b053e560a58b7d0a9d7d9823eaa2e4cde6e290
Kiash254 <codewithkiash@gmail.com>
2023-09-27 21:07:42 +0300

    added blog page

--- ecommerce/urls.py
+++ ecommerce/urls.py
@@ -8,5 +8,6 @@ admin.site.site_title='Admin Ecom Shop'
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('',include('core.urls',namespace='core')),
-    path('aboutus/',include('aboutus.urls',namespace='aboutus'))
+    path('aboutus/',include('aboutus.urls',namespace='aboutus')),
+    path('blog/',include('blog.urls',namespace='blog'))
 ]
