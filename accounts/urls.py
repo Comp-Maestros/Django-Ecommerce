8ab6670a3f704244b11f3c5d75dfe02e75bf24b9
Kiash254 <codewithkiash@gmail.com>
2023-10-02 18:40:57 +0300

    added migrations

--- accounts/urls.py
+++ accounts/urls.py
@@ -1,8 +1,9 @@
 from django.urls import path    
-from .views import RegisterView
+from .views import RegisterView,LoginView
 
 app_name='accounts'
 
 urlpatterns=[
-    path('',RegisterView,name='register')
+    path('',RegisterView,name='register'),
+    path('login/',LoginView,name='login'),
 ]
