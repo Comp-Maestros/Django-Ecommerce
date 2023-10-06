4aa6e47b4f457f78f96b04306322ae311f41450e
Kiash254 <codewithkiash@gmail.com>
2023-10-06 11:27:24 +0300

    added migrations

--- checkout/views.py
+++ checkout/views.py
@@ -3,4 +3,4 @@ from django.shortcuts import render
 # Create your views here.
 def CheckoutView(request):
     
-    return render(request,'checkout/check.html')
+    return render(request,'checkout/checkout.html')
