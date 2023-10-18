3135238d685d41f74e5c79174ff64fff47170abc
Kiash254 <codewithkiash@gmail.com>
2023-10-18 20:43:38 +0300

    added models

--- products/models.py
+++ products/models.py
@@ -4,3 +4,8 @@ from django.db import models
 class Product(models.Model):
     name=models.CharField(max_length=255,blank=True)
     description=models.TextField(max_length=255)
+    image=models.ImageField(upload_to='products')
+    price=models.IntegerField()
+    
+    def __str__ (self):
+        return self.name
