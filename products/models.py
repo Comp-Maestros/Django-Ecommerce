e91060618e687e51e39881fdab3ca28b34353253
Kiash254 <codewithkiash@gmail.com>
2023-10-22 00:24:46 +0300

    added more database logic

--- products/models.py
+++ products/models.py
@@ -8,8 +8,12 @@ class Item(models.Model):
     description = models.TextField()
     image_1 = models.ImageField(upload_to='images/')
     image_2 = models.ImageField(upload_to='images/')
-    image_1_thumbnail = ImageSpecField(source='image_1', processors=[ResizeToFit(300, 300)], format='JPEG', options={'quality': 90})
-    image_2_thumbnail = ImageSpecField(source='image_2', processors=[ResizeToFit(300, 300)], format='JPEG', options={'quality': 90})
+    image_1_thumbnail = ImageSpecField(source='image_1', 
+                                       processors=[ResizeToFit(600, 400)],
+                                       format='JPEG', options={'quality': 60})
+    image_2_thumbnail = ImageSpecField(source='image_2',
+                                       processors=[ResizeToFit(600, 400)], 
+                                       format='JPEG', options={'quality': 60})
     price = models.DecimalField(max_digits=10, decimal_places=2)
 
     def __str__(self):
