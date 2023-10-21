042b92b793256e64d0d28eb229caea9eaa2a538d
Kiash254 <codewithkiash@gmail.com>
2023-10-22 00:17:54 +0300

    /added more products

--- products/models.py
+++ products/models.py
@@ -1,4 +1,6 @@
 from django.db import models
+from imagekit.models import ImageSpecField
+from imagekit.processors import ResizeToFit
 
     
 class Item(models.Model):
@@ -6,6 +8,8 @@ class Item(models.Model):
     description = models.TextField()
     image_1 = models.ImageField(upload_to='images/')
     image_2 = models.ImageField(upload_to='images/')
+    image_1_thumbnail = ImageSpecField(source='image_1', processors=[ResizeToFit(300, 300)], format='JPEG', options={'quality': 90})
+    image_2_thumbnail = ImageSpecField(source='image_2', processors=[ResizeToFit(300, 300)], format='JPEG', options={'quality': 90})
     price = models.DecimalField(max_digits=10, decimal_places=2)
 
     def __str__(self):
