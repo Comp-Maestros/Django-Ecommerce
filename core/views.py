54bfb806acc6a7f146ab1e5b4273fcac311dd3e6
Kiash254 <codewithkiash@gmail.com>
2023-11-02 17:16:15 +0300

    added popular items

--- core/views.py
+++ core/views.py
@@ -1,10 +1,12 @@
 from django.shortcuts import render
-from products.models import Item
+from products.models import Item,Popular
 # Create your views here.
 def HomeView(request):
     items=Item.objects.all()
+    popular=Popular.objects.all()
     context={
-        'items':items
+        'items':items,
+        'popular':popular,
     }
     
     return render(request,'index.html',context)
